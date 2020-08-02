6b59f92490eae4db8120f5c4177e82d87e6ffa3e
PaolaGarciaP <pgarciap15@gmail.com>
2020-08-01 22:13:11 -0500

    Configuration redis

---
+++ RouletteApi/RouletteApi/Controllers/RouletteController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+
+namespace RouletteApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RouletteController : ControllerBase
+    {
+        private readonly IDatabase _database;
+        public RouletteController(IDatabase database)
+        {
+            _database = database;
+        }
+
+
+        [HttpGet ]
+        public string Get([FromQuery]string key)
+        {
+            return _database.StringGet(key);
+        }
+
+        [HttpPost]
+        public void Post([FromQuery] KeyValuePair<string,string> keyValue)
+        {
+            _database.StringSet(keyValue.Key ,keyValue.Value );
+        }
+
+    }
+}
